6f2d8fa1cf6e7130161dc91fd41687e4650da211
NadiasCodes <De.nadia@outlook.com>
2024-04-10 13:14:23 +0200

    learned how to use keyframes

--- src/App.jsx
+++ src/App.jsx
@@ -41,14 +41,23 @@ const App = () => {
         transition={{duration:1, ease: "easeOut", delay:0.4}}
         /> 
        </motion.div>
-        
+
+
+        {/* Shape Shifting */}
         <motion.div 
         variants={gridSquareVariants} 
-        className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10">
         
         <motion.div 
-        variants={gridSquareVariants} 
-        className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></motion.div>
+        className="w-1/3 h-1/3 shadow-md bg-rose-400"
+        animate={{
+          scale: [1,2,2,1],
+          rotate: [0,90,90,0],
+          borderRadius: ["10%","10%","50%","10%"],
+        }}
+        transition= {{duration:5, ease: "easeInOut", repeat: Infinity, repeatDelay:1 }} />
+
+        </motion.div>
         
         <motion.div 
         variants={gridSquareVariants} 
